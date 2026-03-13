80d355a4ea8fc7a940749d983d1cdeedbc7a548f
Milkah Mwangi <gathonimilkahh@gmail.com>
2026-03-13 17:28:32 +0300

    I learned how post, patch, and delete work practically in code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Simulated data
+
 class Event:
     def __init__(self, id, title):
         self.id = id
@@ -11,44 +11,41 @@ class Event:
     def to_dict(self):
         return {"id": self.id, "title": self.title}
 
-# In-memory "database"
+
 events = [
     Event(1, "Tech Meetup"),
     Event(2, "Python Workshop")
 ]
 
-# TODO: Task 1 - Define the Problem
-# Create a new event from JSON input
+
 @app.route("/events", methods=["POST"])
 def create_event():
-    # TODO: Task 2 - Design and Develop the Code
+    data = request.get_json()
+    new_id = max((e.id for e in events), default = 0) + 1
+    event = Event(id=new_id, title=data["title"])
+    events.append(event)
+    return jsonify(event.to_dict()), 201
 
-    # TODO: Task 3 - Implement the Loop and Process Each Element
 
-    # TODO: Task 4 - Return and Handle Results
-    pass
-
-# TODO: Task 1 - Define the Problem
-# Update the title of an existing event
 @app.route("/events/<int:event_id>", methods=["PATCH"])
 def update_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
+    data = request.get_json()
+    event = next((e.to_dict() for e in events if e.id == event_id), None)
+    if not event:
+        return("Event not found!", 404)
+    if "title" in data:
+        event["title"] = data["title"]
+    return jsonify(event), 200
 
-    # TODO: Task 3 - Implement the Loop and Process Each Element
 
-    # TODO: Task 4 - Return and Handle Results
-    pass
-
-# TODO: Task 1 - Define the Problem
-# Remove an event from the list
 @app.route("/events/<int:event_id>", methods=["DELETE"])
 def delete_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
-
-    # TODO: Task 3 - Implement the Loop and Process Each Element
-
-    # TODO: Task 4 - Return and Handle Results
-    pass
+    global events
+    event = next((e for e in events if e.id == event_id), None)
+    if not event:
+        return("Event not found", 404)
+    events = [e for e in events if e.id != event_id]
+    return("Event deleted successfully", 204)
 
 if __name__ == "__main__":
     app.run(debug=True)
